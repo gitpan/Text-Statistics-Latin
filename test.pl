e3ca309c2c7a1fa7618289498f53d46dc5d4b3bc
Rodrigo Panchiniak Fernandes <fernandes@cpan.org>
2007-07-06 18:27:44 +0000

    Import of FERNANDES/Text-Statistics-Latin-0.02 from CPAN.
    gitpan-cpan-distribution: Text-Statistics-Latin
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         FERNANDES/Text-Statistics-Latin-0.02.tar.gz
    gitpan-cpan-author:       FERNANDES
    gitpan-cpan-maturity:     released

---
+++ test.pl
@@ -0,0 +1,14 @@
+# Before `make install' is performed this script should be runnable with
+# `make test'. After `make install' it should work as `perl Text-Statistics-Latin.t'
+
+#########################
+
+
+use Text::Statistics::Latin; #isto segue o simplícimo exemplo do módulo Quine
+
+
+#########################
+
+# Insert your test code below, the Test::More module is use()ed here so read
+# its man page ( perldoc Test::More ) for help writing this test script.
+
